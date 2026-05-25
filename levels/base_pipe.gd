42736c54abcee12e1b7b54cf5db06bb70af612e8
koriplay2m <nyawonathaniel@gmail.com>
2026-05-25 12:02:34 -0600

    Background added

---
+++ levels/base_pipe.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+
+#---Physics Process Function-------------------------------------------
+func _physics_process(delta: float) -> void:
+	if get_parent() is PathFollow2D:
+		if get_parent().progress_ratio >= 1.0:
+			queue_free()
