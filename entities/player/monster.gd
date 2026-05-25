e141f909acf019934fd6dd6021764b091136ba2a
koriplay2m <nyawonathaniel@gmail.com>
2026-05-25 11:24:17 -0600

    gravity increase

--- entities/player/monster.gd
+++ entities/player/monster.gd
@@ -3,8 +3,7 @@ extends CharacterBody2D
 
 const JUMP_VELOCITY = -500.0
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")*1.75
-
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")*4.0
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
