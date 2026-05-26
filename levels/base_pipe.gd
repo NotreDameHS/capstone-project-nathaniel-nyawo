e23e12073a3fb66d81d8e1e13531bc5f8be7e48c
Db01z <danoti361@gmail.com>
2026-05-26 10:31:13 -0600

    Pipe spawn

--- levels/base_pipe.gd
+++ levels/base_pipe.gd
@@ -1,6 +1,6 @@
 extends Area2D
-
-
+@onready var target_path: Path2D
+@export var mob_types : Array[PackedScene]
 #---Physics Process Function-------------------------------------------
 func _physics_process(delta: float) -> void:
 	if get_parent() is PathFollow2D:
