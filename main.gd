7342464d1a9e46d2b263bde75ba78ce76213e079
Db01z <danoti361@gmail.com>
2026-05-28 13:58:52 -0600

    thursday

--- main.gd
+++ main.gd
@@ -1,8 +1,10 @@
 extends Node2D
 @onready var spawn_point = $spawn_point
 @export var pipe_scene : PackedScene
+@onready var timer = $spawn_point/Timer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	pass # Replace with function body.
 
 
@@ -15,8 +17,8 @@ func _on_timer_timeout() -> void:
 	var new_pipe = pipe_scene.instantiate()
 	add_child(new_pipe)
 	new_pipe.global_position.x = spawn_point.global_position.x
-	new_pipe.global_position.y = randf_range(200, 400)
-	add_child(new_pipe)
+	new_pipe.global_position.y = randf_range(200, 500)
+	
 	pass # Replace with function body.
 
 
