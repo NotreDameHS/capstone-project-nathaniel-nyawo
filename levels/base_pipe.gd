6868f5005440ab5d50a525d7dd0573129574dfc3
koriplay2m <nyawonathaniel@gmail.com>
2026-06-03 14:15:24 -0600

    ERRYTHING GONE

--- levels/base_pipe.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-@onready var target_path: Path2D
-@export var mob_types : Array[PackedScene]
-#---Physics Process Function-------------------------------------------
-func _physics_process(delta: float) -> void:
-	if get_parent() is PathFollow2D:
-		if get_parent().progress_ratio >= 1.0:
-			queue_free()
