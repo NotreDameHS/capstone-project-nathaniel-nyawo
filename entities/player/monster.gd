26c8d62238e7c49b812fa8b58b7a22c445b384ad
koriplay2m <nyawonathaniel@gmail.com>
2026-06-03 14:12:24 -0600

    done w this

--- entities/player/monster.gd
+++ entities/player/monster.gd
@@ -1,16 +1,1 @@
-extends CharacterBody2D
-
-
-const JUMP_VELOCITY = -300.0
-
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")*4.0
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = JUMP_VELOCITY
-
-	move_and_slide()
+extends Area2D
