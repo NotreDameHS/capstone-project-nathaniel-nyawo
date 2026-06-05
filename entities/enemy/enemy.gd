26b9f7f158336a9182c22ee2672737f89fc4ceea
koriplay2m <nyawonathaniel@gmail.com>
2026-06-05 11:33:56 -0600

    mob health bar (needs debugging)

---
+++ entities/enemy/enemy.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+@export var path_speed: float = 0.2
+@export var chase_speed: float = 120.0 
+var is_chasing: bool = false
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+###-Health Variables-------------------------------------------------------------------------
+@export var max_health := 200.0
+@export var health = max_health
+@onready var health_bar = $UI/HealthBar
+@onready var ui_node = $UI
+
+#---On Ready---------------------------------------------------------------------------------
+func _on_ready() -> void:
+	health = max_health
+	
+	health_bar.max_value = max_health
+	health_bar.value = health
+	
+#---Taking Damage----------------------------------------------------------------------------
+func _take_damage(amount: float) -> void:
+	health -= amount
+	if health <= 0.0:
+		health = 0.0
+		queue_free()
+	health_bar.value = health
+
+func _physics_process(delta: float) -> void:
+	var player = get_tree().current_scene.get_node_or_null("Monster")
+	
+	health_bar.rotation = global_rotation
+	ui_node.rotation = global_rotation
+
+	if player:
+		var direction = global_position.direction_to(player.global_position)
+		velocity = direction * chase_speed
+		move_and_slide()
