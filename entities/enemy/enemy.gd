a6e140fb6a730a6fe8ac9a641dd2e923ef998bba
Db01z <danoti361@gmail.com>
2026-06-07 20:53:17 -0600

    updated enemy gd to make collectables and damage
    updated enemy

--- entities/enemy/enemy.gd
+++ entities/enemy/enemy.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
-@export var path_speed: float = 0.2
 @export var chase_speed: float = 120.0 
+@export var damage_amount: float = 10.0
 var is_chasing: bool = false
 
 const SPEED = 300.0
@@ -15,18 +15,27 @@ const JUMP_VELOCITY = -400.0
 #---On Ready---------------------------------------------------------------------------------
 func _on_ready() -> void:
 	health = max_health
-	
-	health_bar.max_value = max_health
-	health_bar.value = health
+	if health_bar:
+		
+		health_bar.max_value = max_health
+		health_bar.value = health
 	
 #---Taking Damage----------------------------------------------------------------------------
 func _take_damage(amount: float) -> void:
 	health -= amount
 	if health <= 0.0:
+		GameManager.add_score(15)
+		spawn_collectible_drop()
 		health = 0.0
 		queue_free()
-	health_bar.value = health
-
+	if health_bar:
+		health_bar.value = health
+func spawn_collectible_drop() -> void:
+	var drop_scene = load("://entities/collectibles/speed_boost/speed.tscn")
+	if drop_scene:
+		var drop = drop_scene.instantiate()
+		get_parent().call_deferred("add_child", drop)
+		drop.global_postion = global_position
 func _physics_process(delta: float) -> void:
 	var player = get_tree().current_scene.get_node_or_null("Monster")
 	
@@ -37,3 +46,8 @@ func _physics_process(delta: float) -> void:
 		var direction = global_position.direction_to(player.global_position)
 		velocity = direction * chase_speed
 		move_and_slide()
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Monster":
+		if body.has_method("take_damage"):
+			body.take_damage(damage_amount)
+		queue_free()
