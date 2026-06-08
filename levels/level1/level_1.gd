65c348423205fbc4ef05f9e8185bb2face30c604
Db01z <danoti361@gmail.com>
2026-06-07 21:02:26 -0600

    Update level_1.gd
    added waves to level 1 gd

--- levels/level1/level_1.gd
+++ levels/level1/level_1.gd
@@ -2,7 +2,7 @@ extends Node2D
 @export var enemy_scene: PackedScene
 @export var max_enemies : int = 10
 @onready var spawn_timer: Timer = $enemyspawntimer
-
+var total_spawned_this_wave: int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawn_timer.start()
@@ -17,6 +17,13 @@ func _process(delta: float) -> void:
 func _on_enemyspawntimer_timeout() -> void:
 	if not enemy_scene:
 		return
+	var wave_limit = max_enemies + (GameManager.current_wave * 5)
+	if total_spawned_this_wave >= wave_limit:
+		var alive_mobs = get_tree().get_nodes_in_group("mobs").size()
+		if alive_mobs == 0:
+			total_spawned_this_wave = 0
+			GameManager.advance_wave()
+		return
 	var current_enemy_count = get_tree().get_nodes_in_group("mobs").size()
 	if current_enemy_count >= max_enemies:
 		return
@@ -25,6 +32,7 @@ func _on_enemyspawntimer_timeout() -> void:
 	$"spawn path/PathFollow2D".progress_ratio = randf()
 	new_enemy.global_position = $"spawn path/PathFollow2D".global_position
 	add_child(new_enemy)
+	total_spawned_this_wave += 1
 	
 func _on_game_over() -> void:
 	spawn_timer.stop()
